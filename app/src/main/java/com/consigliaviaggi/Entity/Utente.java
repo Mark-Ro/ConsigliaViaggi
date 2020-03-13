e21ce63a9df186bee97ba56bfb9b6334c66a6056
Mark-Ro <marco.romano9898@gmail.com>
2020-03-13 18:39:18 +0100

    Getter and Setter Utente

--- app/src/main/java/com/consigliaviaggi/Entity/Utente.java
+++ app/src/main/java/com/consigliaviaggi/Entity/Utente.java
@@ -18,6 +18,74 @@ public class Utente {
         return istance;
     }
 
+    public int getNumeroRecensioni() {
+        return numeroRecensioni;
+    }
+
+    public void setNumeroRecensioni(int numeroRecensioni) {
+        this.numeroRecensioni = numeroRecensioni;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getStatoAccount() {
+        return statoAccount;
+    }
+
+    public void setStatoAccount(String statoAccount) {
+        this.statoAccount = statoAccount;
+    }
+
+    public String getNomePubblico() {
+        return nomePubblico;
+    }
+
+    public void setNomePubblico(String nomePubblico) {
+        this.nomePubblico = nomePubblico;
+    }
+
+    public float getMedia() {
+        return media;
+    }
+
+    public void setMedia(float media) {
+        this.media = media;
+    }
+
+    public void setUtenteAutenticato(boolean utenteAutenticato) {
+        this.utenteAutenticato = utenteAutenticato;
+    }
+
     public boolean isUtenteAutenticato() {
         return utenteAutenticato;
     }

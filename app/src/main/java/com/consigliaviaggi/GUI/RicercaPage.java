ca0052bdd4f4f6c5a85852878d002bdf598a7281
Mark-Ro <marco.romano9898@gmail.com>
2020-03-13 18:51:39 +0100

    Aggiunta RicercaPage

---
+++ app/src/main/java/com/consigliaviaggi/GUI/RicercaPage.java
@@ -0,0 +1,16 @@
+package com.consigliaviaggi.GUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.consigliaviaggi.R;
+
+public class RicercaPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ricerca_page);
+    }
+}

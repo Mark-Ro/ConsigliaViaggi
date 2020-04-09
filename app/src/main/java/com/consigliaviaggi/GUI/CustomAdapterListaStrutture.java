a0da64e2f55613172f969e189f727d5f7dbc6ea2
Massi <massimiliano.russo.unina@gmail.com>
2020-04-09 21:17:30 +0200

    Migliorata Grafica lista strutture

--- app/src/main/java/com/consigliaviaggi/GUI/CustomAdapterListaStrutture.java
+++ app/src/main/java/com/consigliaviaggi/GUI/CustomAdapterListaStrutture.java
@@ -54,22 +54,63 @@ public class CustomAdapterListaStrutture extends BaseAdapter {
 
         Picasso.get().load(arrayList.get(position).getFotoStruttura()).noFade().fit().centerCrop().into(viewHolder.imageViewStruttura);
         viewHolder.textViewNomeStruttura.setText(arrayList.get(position).getNomeStruttura());
-        viewHolder.textViewVoto.setText(String.valueOf(arrayList.get(position).getVoto()));
         if (arrayList.get(position).getDistanza()>=0)
             viewHolder.textViewDistanza.setText(String.valueOf(arrayList.get(position).getDistanza()) + "m");
+        switch (arrotondaNumero(arrayList.get(position).getVoto())){
+
+            case "1":
+                viewHolder.imageViewStelle.setImageResource(R.drawable.ic_1stelle);
+                break;
+            case "1.5":
+                viewHolder.imageViewStelle.setImageResource(R.drawable.ic_1_2stelle);
+                break;
+            case "2":
+                viewHolder.imageViewStelle.setImageResource(R.drawable.ic_2stelle);
+                break;
+            case "2.5":
+                viewHolder.imageViewStelle.setImageResource(R.drawable.ic_2_2stelle);
+                break;
+            case "3":
+                viewHolder.imageViewStelle.setImageResource(R.drawable.ic_3stelle);
+                break;
+            case "3.5":
+                viewHolder.imageViewStelle.setImageResource(R.drawable.ic_3_2stelle);
+                break;
+            case "4":
+                viewHolder.imageViewStelle.setImageResource(R.drawable.ic_4stelle);
+                break;
+            case "4.5":
+                viewHolder.imageViewStelle.setImageResource(R.drawable.ic_4_2stelle);
+                break;
+            case "5":
+                viewHolder.imageViewStelle.setImageResource(R.drawable.ic_5stelle);
+                break;
+
+        }
 
         return r;
     }
 
     class ViewHolder {
-        ImageView imageViewStruttura;
-        TextView textViewNomeStruttura,textViewVoto,textViewDistanza;
+        ImageView imageViewStruttura,imageViewStelle;
+        TextView textViewNomeStruttura,textViewDistanza;
 
         public ViewHolder(View view) {
             imageViewStruttura = view.findViewById(R.id.imageViewStruttura);
             textViewNomeStruttura = view.findViewById(R.id.textViewNomeStruttura);
-            textViewVoto = view.findViewById(R.id.textViewVoto);
+            imageViewStelle = view.findViewById(R.id.imageViewStella);
             textViewDistanza = view.findViewById(R.id.textViewDistanza);
         }
+    }
+    private String arrotondaNumero(float numero){
+        int decimale= Integer.parseInt(String.valueOf(numero).substring(2,3));
+        String risultato;
+        if(decimale<5 || decimale>5){
+            risultato=String.valueOf(Math.round(numero));
+        }
+        else{
+            risultato=String.valueOf(numero).substring(0,3);
+        }
+        return risultato;
     }
 }

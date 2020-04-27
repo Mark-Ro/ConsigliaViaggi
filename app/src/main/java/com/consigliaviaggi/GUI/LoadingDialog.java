1ffc87556618104d5a7f77c86017debb5f5e4dda
Massi <massimiliano.russo.unina@gmail.com>
2020-04-27 20:28:03 +0200

    Aggiunta animazione dialog loading

--- app/src/main/java/com/consigliaviaggi/GUI/LoadingDialog.java
+++ app/src/main/java/com/consigliaviaggi/GUI/LoadingDialog.java
@@ -3,7 +3,10 @@ package com.consigliaviaggi.GUI;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.consigliaviaggi.R;
 
 public class LoadingDialog {
@@ -17,12 +20,14 @@ public class LoadingDialog {
     public void startLoadingDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         LayoutInflater inflater = activity.getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.loading_dialog,null));
+        View view= inflater.inflate(R.layout.loading_dialog,null);
+        builder.setView(view);
         builder.setCancelable(true);
-
         alertDialog = builder.create();
         alertDialog.setCancelable(false);
         alertDialog.setCanceledOnTouchOutside(false);
+        ImageView imageViewLoading=view.findViewById(R.id.imageViewLoading);
+        Glide.with(activity).asGif().load(R.raw.gif_loading).into(imageViewLoading);
         alertDialog.show();
     }
 
